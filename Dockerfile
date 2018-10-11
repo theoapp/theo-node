21bba9041045950c17d4258cefb98f164ceee098
Michele Azzolari <michele@azzolari.it>
2018-10-11 22:02:41 +0200

    Improvment in docker image build

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,7 @@ WORKDIR /usr/src/app
 
 ARG NODE_ENV
 ENV NODE_ENV $NODE_ENV
-COPY package.json /usr/src/app/
-COPY package-lock.json /usr/src/app/
-RUN npm install --no-optional
+
 COPY . /usr/src/app
-RUN npm run build
 
 CMD [ "npm", "start" ]
--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+DOCKER_IMAGE=theoapp/theo
 
-PACKAGE_VERSION=$(cat package.json \
+DOCKER_TAG=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-echo $PACKAGE_VERSION
+echo $DOCKER_TAG
 rm -rf build node_modules
 docker run --rm -v $PWD:/usr/local/src -w /usr/local/src node:8-alpine sh -c 'npm i -g npm && npm i --no-optional && npm run build' 
 
-# docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
+docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
 
