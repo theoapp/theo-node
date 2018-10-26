f4b90b0368ff93aa2defb95729259cab76429b42
Michele Azzolari <michele@azzolari.it>
2018-10-26 17:58:07 +0200

    We return plain/text (default) or application/json as requested in Accept header

--- src/routes/keys.js
+++ src/routes/keys.js
@@ -1,13 +1,20 @@
 import { requireAuthMiddleware } from '../lib/middlewares/AuthMiddleware';
-import { getAuthorizedKeys } from '../lib/helpers/KeysHelper';
+import { getAuthorizedKeys, getAuthorizedKeysAsJson } from '../lib/helpers/KeysHelper';
 
 export default function handleKeys(server) {
   server.get('/authorized_keys/:host/:user', requireAuthMiddleware, async (req, res, next) => {
+    const accept = req.header('Accept');
+
     const { host, user } = req.params;
     try {
-      const keys = await getAuthorizedKeys(req.db, user, host);
-      res.header('Content-Type', 'text/plain');
-      res.send(keys);
+      if (accept && accept.indexOf('application/json') >= 0) {
+        const keys = await getAuthorizedKeysAsJson(req.db, user, host);
+        res.json(keys);
+      } else {
+        const keys = await getAuthorizedKeys(req.db, user, host);
+        res.header('Content-Type', 'text/plain');
+        res.send(keys);
+      }
     } catch (err) {
       res.status(err.t_code || 500);
       res.json({ status: err.t_code || 500, reason: err.message });
