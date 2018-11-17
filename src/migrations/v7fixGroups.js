874d9a741e6db994ae888df6051a22ef17f01fc3
Michele Azzolari <michele@azzolari.it>
2018-11-17 09:07:49 +0100

    Fix bug whan upgrading db from version < 7

--- src/migrations/v7fixGroups.js
+++ src/migrations/v7fixGroups.js
@@ -1,14 +1,16 @@
-import { adminCreateGroup, adminCreateGroupAccount, adminGetAccount } from '../lib/helpers/AdminHelper';
+import { adminCreateGroup, adminCreateGroupAccount } from '../lib/helpers/AdminHelper';
 
 export const runV7migrationMariaDb = async client => {
   const sql = 'select distinct account_id from permissions where group_id is null';
   const accountsToFix = await client.all(sql);
   for (let i = 0; i < accountsToFix.length; i++) {
-    const account = await adminGetAccount(client, accountsToFix[i].account_id);
+    const sqlAccount = 'select email from accounts where id = ?';
+    const account_id = accountsToFix[i].account_id;
+    const account = await client.get(sqlAccount, [account_id]);
     const group_id = await adminCreateGroup(client, { name: account.email }, true);
     await adminCreateGroupAccount(db, group_id, account_id);
     const updateSql = 'update permissions set group_id = ? where account_id = ? ';
-    await client.run(updateSql, [group_id, account.id]);
+    await client.run(updateSql, [group_id, account_id]);
   }
   try {
     const dropFKSql = 'alter table permissions DROP FOREIGN KEY permissions_account_id';
@@ -25,10 +27,15 @@ export const runV7migrationSqliteDb = async client => {
   const sql = 'select distinct account_id from permissions where group_id is null';
   const accountsToFix = await client.all(sql);
   for (let i = 0; i < accountsToFix.length; i++) {
-    const account = await adminGetAccount(client, accountsToFix[i].account_id);
+    const account_id = accountsToFix[i].account_id;
+    const sqlAccount = 'select email from accounts where id = ?';
+    const account = await client.get(sqlAccount, [account_id]);
+    console.log('Creating group ', account.email);
     const group_id = await adminCreateGroup(client, { name: account.email }, true);
-    await adminCreateGroupAccount(db, group_id, account_id);
+    console.log('Associating group %s with account %s', group_id, account_id);
+    await adminCreateGroupAccount(client, group_id, account_id);
+    console.log('Setting permissions to group %s from account %s', group_id, account_id);
     const updateSql = 'update permissions set group_id = ? where account_id = ? ';
-    await client.run(updateSql, [group_id, account.id]);
+    await client.run(updateSql, [group_id, account_id]);
   }
 };
