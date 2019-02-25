6b1b28a8d32fc62d66db9e649fd49b26ade6ebdd
Michele Azzolari <michele@azzolari.it>
2019-02-25 11:20:46 +0100

    Tests admin multi tokens

--- src/test/testCore.js
+++ src/test/testCore.js
@@ -20,6 +20,13 @@ const tokensTwo = {
   }
 };
 
+const tokensThree = {
+  tokens: {
+    admins: ['q1q1q1q1q1q1q1q1', 'p0p0p0p0p0p0'],
+    clients: ['w2w2w2w2w2w2w2w2w2w2', 'e3e3e3e3e3e3e3e3e3e3e3e']
+  }
+};
+
 describe('Core', function() {
   describe('Send tokens', function() {
     it('should return 204', async function() {
@@ -179,6 +186,134 @@ describe('Core', function() {
         method: 'GET',
         headers: {
           Authorization: 'Bearer ' + tokensTwo.tokens.clients[1]
+        }
+      });
+      assert.strictEqual(res.status, 200);
+    });
+  });
+
+  describe('Resend tokens 3', function() {
+    it('should return 204', async function() {
+      const res = await fetch(base_url + '/tokens', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + process.env.CORE_TOKEN,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tokensThree)
+      });
+      assert.strictEqual(res.status, 204);
+    });
+  });
+
+  describe('Recheck tokens', function() {
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/tokens', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + tokensOne.tokens.admin,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tokensOne)
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/accounts', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensOne.tokens.admin
+        }
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/accounts', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensTwo.tokens.clients[0]
+        }
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensOne.tokens.admin
+        }
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensTwo.tokens.admin
+        }
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensTwo.tokens.clients[0]
+        }
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 401', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensTwo.tokens.clients[1]
+        }
+      });
+      assert.strictEqual(res.status, 401);
+    });
+
+    it('should return 200', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensThree.tokens.admins[0]
+        }
+      });
+      assert.strictEqual(res.status, 200);
+    });
+
+    it('should return 200', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensThree.tokens.admins[1]
+        }
+      });
+      assert.strictEqual(res.status, 200);
+    });
+
+    it('should return 200', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensThree.tokens.clients[0]
+        }
+      });
+      assert.strictEqual(res.status, 200);
+    });
+
+    it('should return 200', async function() {
+      const res = await fetch(base_url + '/authorized_keys/host/user', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + tokensThree.tokens.clients[1]
         }
       });
       assert.strictEqual(res.status, 200);
