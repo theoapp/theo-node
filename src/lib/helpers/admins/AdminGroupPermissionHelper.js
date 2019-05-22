b9d953992338acb243c8de57e3e316f1251e63f1
Michele Azzolari <michele@azzolari.it>
2019-05-22 17:32:14 +0200

    Audit: add audit to groups functions

---
+++ src/lib/helpers/admins/AdminGroupPermissionHelper.js
@@ -0,0 +1,77 @@
+import GroupManager from '../../managers/GroupManager';
+import PermissionManager from '../../managers/PermissionManager';
+import AuditHelper from '../AuditHelper';
+
+export const adminAddGroupPermission = async (db, group_id, user, host, auth_token) => {
+  if (!user) {
+    const error = new Error('Malformed object, user is required');
+    error.code = 400;
+    throw error;
+  }
+  if (!host) {
+    const error = new Error('Malformed object, host is required');
+    error.code = 400;
+    throw error;
+  }
+  const gm = new GroupManager(db);
+  let group;
+  try {
+    if (isNaN(group_id)) {
+      group = await gm.getByName(group_id);
+      group_id = group.id;
+    } else {
+      group = await gm.get(group_id);
+    }
+  } catch (err) {
+    err.t_code = 404;
+    console.log('Throw 404');
+    throw err;
+  }
+  const pm = new PermissionManager(db);
+  try {
+    const permission_id = await pm.create(group_id, user, host);
+    AuditHelper.log(auth_token, 'group', 'add_permission', group.name, { host, user });
+    return { group_id, permission_id };
+  } catch (err) {
+    err.t_code = 500;
+    throw err;
+  }
+};
+
+export const adminDeleteGroupPermission = async (db, group_id, permission_id, auth_token) => {
+  const gm = new GroupManager(db);
+  let group;
+  try {
+    if (isNaN(group_id)) {
+      group = await gm.getByName(group_id);
+      group_id = group.id;
+    } else {
+      group = await gm.get(group_id);
+    }
+  } catch (err) {
+    err.t_code = 404;
+    console.log('Throw 404');
+    throw err;
+  }
+  const pm = new PermissionManager(db);
+  try {
+    const permission = await pm.get(group_id, permission_id);
+    if (!permission) {
+      const error = new Error('Permission not found');
+      error.t_code = 404;
+      throw error;
+    }
+    const ret = await pm.delete(group_id, permission_id);
+    if (ret === 0) {
+      const error = new Error('Permission not found');
+      error.t_code = 404;
+      throw error;
+    }
+    const { host, user } = permission;
+    AuditHelper.log(auth_token, 'group', 'remove_permission', group.name, { host, user });
+    return true;
+  } catch (err) {
+    if (!err.t_code) err.t_code = 500;
+    throw err;
+  }
+};
