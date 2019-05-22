35dd19c78f76b5519d0c3d26ad0addf8de2c31e7
Michele Azzolari <michele@azzolari.it>
2019-05-22 22:51:13 +0200

    Fix typos

--- src/lib/helpers/RemoteLoggerHelper.js
+++ src/lib/helpers/RemoteLoggerHelper.js
@@ -15,7 +15,7 @@ class RemoteLoggerHelper {
   static log(data) {
     if (!LOG_AUTH_KEYS_URL) return;
     setImmediate(() => {
-      http_post(LOG_AUTH_KEYS_URL, data, this.getHttpHeaders()).cache(e => {
+      http_post(LOG_AUTH_KEYS_URL, data, this.getHttpHeaders()).catch(e => {
         console.error('Failed to POST log: %s', e.message, data);
       });
     });
