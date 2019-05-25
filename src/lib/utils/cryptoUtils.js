2b32246d418cf6bcb7c0f75ed3d4a521283cf126
Michele Azzolari <michele@azzolari.it>
2019-05-25 17:00:26 +0200

    Extends crypotUtils

--- src/lib/utils/cryptoUtils.js
+++ src/lib/utils/cryptoUtils.js
@@ -1,8 +1,16 @@
 import crypto from 'crypto';
 
 export const md5 = function(data) {
+  return hash('md5', data);
+};
+
+export const sha256 = function(data) {
+  return hash('sha256', data);
+};
+
+export const hash = function(type, data) {
   return crypto
-    .createHash('md5')
+    .createHash(type)
     .update(data)
     .digest('hex');
 };
