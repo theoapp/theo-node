bfd5ffb2a536ba3ec277667a1184c14707feeda8
Michele Azzolari <michele@azzolari.it>
2019-08-26 15:56:54 +0200

    Utils: new funtion to get human readable elapsed time from ms

--- src/lib/utils/dateUtils.js
+++ src/lib/utils/dateUtils.js
@@ -22,3 +22,31 @@ export const getTimestampFromISO8601 = string => {
   }
   return ret;
 };
+
+export function millisecondsToStr(milliseconds) {
+  function numberEnding(number) {
+    return number > 1 ? 's' : '';
+  }
+  let temp = Math.floor(milliseconds / 1000);
+  const years = Math.floor(temp / 31536000);
+  if (years) {
+    return years + ' year' + numberEnding(years);
+  }
+  const days = Math.floor((temp %= 31536000) / 86400);
+  if (days) {
+    return days + ' day' + numberEnding(days);
+  }
+  const hours = Math.floor((temp %= 86400) / 3600);
+  if (hours) {
+    return hours + ' hour' + numberEnding(hours);
+  }
+  const minutes = Math.floor((temp %= 3600) / 60);
+  if (minutes) {
+    return minutes + ' minute' + numberEnding(minutes);
+  }
+  const seconds = temp % 60;
+  if (seconds) {
+    return seconds + ' second' + numberEnding(seconds);
+  }
+  return 'less than a second';
+}
